abce73cddb5f9dcbc10e480a1ac419232d985fe4
CastroNicolas <maximonicolascastro@gmail.com>
2024-03-14 20:44:40 +0100

    Prueba branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-center font-semibold'>PET PALACE</h1>
+      <h1 className='text-3xl text-center font-semibold'>PET PALACE!</h1>
     </>
   )
 }
