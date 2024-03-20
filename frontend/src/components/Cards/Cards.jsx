434b79234b7ef3485845bfd378dc39e7c41769ff
Fer <fer.cantora90@gmail.com>
2024-03-20 01:09:30 -0300

    Carusel del home

---
+++ frontend/src/components/Cards/Cards.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Cards() {
+  return (
+    <><div>Cards</div><div>Cards</div><div>Cards</div><div>Cards</div>
+    <div>Cards</div><div>Cards</div><div>Cards</div><div>Cards</div>
+    <div>Cards</div><div>Cards</div><div>Cards</div><div>Cards</div></>
+  )
+}
