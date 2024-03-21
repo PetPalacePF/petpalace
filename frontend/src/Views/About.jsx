1b7217e2aa17a828a869f4c1ee9909626ac946d5
nicolaspratg <nicoprat@hotmail.com>
2024-03-21 14:27:42 -0300

    detail y about provisorio

--- frontend/src/Views/About.jsx
+++ frontend/src/Views/About.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Header } from "../components/Header/Header";
+import { NavBar } from "../components/Nav Bar/NavBar";
 
 const About = () => {
   return (
     <div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <NavBar />
+      </div>
       <div>
         <h1>About Us</h1>
         <p></p>
