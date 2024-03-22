fc8e36a680a7878f5d499932321bb636cd42d21a
Facundo Besio <facubesio.08@gmail.com>
2024-03-22 18:12:27 -0300

    Update createBulkProducts

--- backend/src/controllers/Products/createBulkProducts.js
+++ backend/src/controllers/Products/createBulkProducts.js
@@ -1,16 +1,34 @@
 const { Product } = require("../../db");
 const findJsonProducts = require("../../utils/json/findJsonProducts");
-
+const findAllCategories = require("../../controllers/Categories/findAllCategories");
+const createBulkCategories = require("../../controllers/Categories/createBulkCategories");
 
 const createBulkProducts = async () => {
   try {
+    let categories = await findAllCategories();
+    if (categories.length === 0) {
+      await createBulkCategories();
+    }
+
     const jsonProducts = await findJsonProducts();
-    const bulkProducts = await Product.bulkCreate(jsonProducts);
-    return bulkProducts; 
+    for (const jsonProduct of jsonProducts) {
+      const {
+        brand,
+        name,
+        description,
+        price,
+        stock,
+        rating,
+        img,
+        categories,
+      } = jsonProduct;
+      const product = { brand, name, img, description, price, stock, rating };
+      const newProduct = await Product.create(product);
+      await newProduct.addCategories(categories);
+    }
   } catch (error) {
     console.log(error);
   }
-
 };
 
 module.exports = createBulkProducts;
--- backend/src/routes/routerControllers/Products/getProducts.js
+++ backend/src/routes/routerControllers/Products/getProducts.js
@@ -6,9 +6,9 @@ const getProducts = async (req, res) => {
   try {
     let products = await findAllProducts();
     if (products.length === 0) {
-      await createBulkProducts()
+      await createBulkProducts();
       products = await findAllProducts();
-    };
+    }
     return res.status(200).json({ products: formattedProducts(products) });
   } catch (error) {
     res.status(500).json({ error: error.message });
