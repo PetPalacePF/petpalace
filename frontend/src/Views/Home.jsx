4d48694f407a21d3ce91517259f1b11e81e6f534
nicolaspratg <nicoprat@hotmail.com>
2024-03-24 18:38:42 -0300

    card y cards top rated/more products echo

--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -1,4 +1,5 @@
 import Cards from "../components/Cards/Cards";
+import TopRatedCards from "../components/Cards/TopRatedCards";
 import { Footer } from "../components/Footer/Footer";
 import Carousel from "../components/Home/Carusel/Carousel";
 import { Epigraph } from "./Epigraph";
@@ -14,13 +15,13 @@ const Home = () => {
       </div>
       <div className="bg-pink">
         <div className="px-20 relative">
-          <hr className="absolute w-[100px] border-black" />
           <h1 className="relative text-4l font-bold">Top Rated</h1>
-          <Cards />
+          <hr className="absolute w-[100px] border-black" />
+          <TopRatedCards />
         </div>
         <div className="px-20 relative mt-10">
-          <hr className="absolute w-[100px] border-black" />
           <h1 className="relative text-4l font-bold">More Products</h1>
+          <hr className="absolute w-[100px] border-black" />
           <Cards />
         </div>
         <Footer />
--- frontend/src/components/Cards/TopRatedCards.jsx
+++ frontend/src/components/Cards/TopRatedCards.jsx
@@ -1,16 +1,39 @@
+// TopRatedCards.jsx
+import React, { useEffect, useState } from "react";
 import { Card } from "./Card";
 
-const Cards = ({ products }) => {
-  // Assuming 'products' is an array of objects with properties: id, name, image, price, category
-  const displayedProducts = products.slice(0, 4); // Get only the first 4 products
+const TopRatedCards = () => {
+  const [topRatedProducts, setTopRatedProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchTopRatedProducts = async () => {
+      try {
+        // Fetch top rated products from your API or database
+        const response = await fetch("http://localhost:5000/products");
+        const data = await response.json();
+
+        // Sort the products based on their rating in descending order
+        const sortedProducts = data.sort((a, b) => b.Rating - a.Rating);
+
+        // Select the top 4 rated products
+        const topRatedProducts = sortedProducts.slice(0, 4);
+
+        setTopRatedProducts(topRatedProducts);
+      } catch (error) {
+        console.error("Error fetching top rated products:", error);
+      }
+    };
+
+    fetchTopRatedProducts();
+  }, []);
 
   return (
-    <div>
-      <div>
-        {displayedProducts.map((product) => (
-          <Card key={product.id} />
-        ))}
-      </div>
+    <div className="flex flex-col md:flex-row justify-between items-center md:items-start my-5">
+      {topRatedProducts.map((product) => (
+        <Card key={product.Id} product={product} />
+      ))}
     </div>
   );
 };
+
+export default TopRatedCards;
