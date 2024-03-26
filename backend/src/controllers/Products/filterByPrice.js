efcd123b863ade8cec0b99d61df54ec7052b08b0
JoseMartinez0 <joseignacio2534@gmail.com>
2024-03-26 11:41:38 -0300

    filterByPrice

--- backend/src/controllers/Products/filterByPrice.js
+++ backend/src/controllers/Products/filterByPrice.js
@@ -1,21 +1,33 @@
 const { Product, Category } = require("../../db");
+const { Op } = require("sequelize");
 
 const filterByPrice = async (query) => {
-console.log(query);
-  // • Copia de findAllProducts •
-  //   const products = await Product.findAll({
-  //     include: {
-  //       model: Category,
-  //       attributes: ["name"],
-  //       through: {
-  //         attributes: [],
-  //       },
-  //     },
-  //     order: [['id', 'ASC']]
-  //   });
   
-  const products = { name: `Test filterByPrice - query =  ${query}`}
-  return products;
-};
+  const prices = query.map(price => parseInt(price.trim()));
 
+  try {
+    const products = await Product.findAll({
+      where: {
+        price: {
+          [Op.between]: [prices[0], prices[1]]
+        }
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+      order: [["id", "ASC"]],
+    });
+
+    return products;
+  } catch (error) {
+    console.error("Error al filtrar productos por precio:", error);
+    throw new Error("Error al filtrar productos por precio");
+  }
+};
 module.exports = filterByPrice;
