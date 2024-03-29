4a46132aead9338411be97a59831efe9bc9f34b4
CastroNicolas <maximonicolascastro@gmail.com>
2024-03-29 19:39:39 +0100

    Searchbar Complete

--- frontend/src/components/Search Bar/SearchBar.jsx
+++ frontend/src/components/Search Bar/SearchBar.jsx
@@ -1,76 +1,37 @@
 import { useState } from "react";
-import axios from "axios";
-import { Card } from "../Cards/Card";
 import { useNavigate } from "react-router-dom";
+import { getProductsByNameOrBrand, getProductsByNameOrBrandOnchange } from "../../utils/getProductsByNameOrBrand";
 
-export const SearchBar = () => {
+export const SearchBar = ({ setProducts }) => {
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        try {
-            if (search.length > 3) {
-                setLoading(true);
-                setError(null);
-                const response = await axios.get(`http://localhost:5000/products?brand_or_name=${search}`);
-                navigate("/shop");
-                setTimeout(() => {
-                    setSearchResults(response.data);
-                    setLoading(false);
-                }, 1000);
-            }
-        } catch (error) {
-            setError("Error searching for products:", error);
-            setLoading(false);
-        }
+        getProductsByNameOrBrand({ search, setLoading, setError, navigate, setProducts });
         setSearch("");
     };
 
-    const onChange = async (event) => {
-        const newSearch = event.target.value;
-        setSearch(newSearch);
-        try {
-            if (newSearch !== '') {
-                setLoading(true);
-                setError(null);
-                const response = await axios.get(`http://localhost:5000/products?brand_or_name=${search}`);
-                navigate("/shop");
-                setTimeout(() => {
-                    setSearchResults(response.data);
-                    setLoading(false);
-                }, 1000);
-            }
-            else {
-                setSearchResults([]);
-                setLoading(false);
-            }
-        } catch (error) {
-            setError("Error searching for products:", error);
-            setLoading(false);
-        }
+    const onChange = (event) => {
+        setSearch(event.target.value);
+        getProductsByNameOrBrandOnchange({ setLoading, setError, navigate, setProducts, searchResults, search });
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="search"
-                placeholder="Search for products"
-                value={search}
-                onChange={onChange}
-            />
-            <button type="submit" disabled={loading}>🔍</button>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="search"
+                    placeholder="Search for products"
+                    value={search}
+                    onChange={onChange}
+                />
+                <button type="submit" disabled={loading}>🔍</button>
 
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            <ul>
-                {Array.isArray(searchResults) && searchResults.map((product, index) => (
-                    <Card key={index} product={product} />
-                ))}
-            </ul>
-        </form>
+            </form>
+            {/* <SearchResults searchResults={searchResults} loading={loading} error={error} /> */}
+        </div>
     )
 }
