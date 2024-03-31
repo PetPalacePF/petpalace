b8303078135e0847e17be30ded993a47052e5a62
Facundo Besio <facubesio.08@gmail.com>
2024-03-31 15:44:57 -0300

    Purchases - SortUsers in progress

--- backend/src/utils/validators/purchases/activeInputsValidator.js
+++ backend/src/utils/validators/purchases/activeInputsValidator.js
@@ -1,9 +1,9 @@
 const activeInputsValidator = (queryInputs) => {
-  const { filterOrders, filterUsers, sortId } = queryInputs;
+  const { filterOrders, filterUsers, sortId, sortUsers } = queryInputs;
 
   let inputsActive = false;
 
-  if (filterOrders.length > 0 || filterUsers.length > 0 || sortId !== "") {
+  if (filterOrders.length > 0 || filterUsers.length > 0 || sortId !== "" || sortUsers !== ""){
     inputsActive = true;
   }
   return inputsActive;
