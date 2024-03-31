fdfc1c8da2f50beb29ea25125c2d0d08357836cc
CastroNicolas <maximonicolascastro@gmail.com>
2024-03-31 20:16:20 +0200

    Filtters Change Variables

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { SearchBar } from "../Search Bar/SearchBar";
 import { NavBar } from "../Nav Bar/NavBar";
 
-const Header = ({ allCategories, setProducts }) => {
+const Header = ({ allCategories, setProducts, search, setSearch }) => {
   return (
     <>
       <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <Link to='/'>🐾PET PALACE</Link>
         <div className="flex items-center">
-          <SearchBar setProducts={setProducts} />
+          <SearchBar setProducts={setProducts} search={search} setSearch={setSearch} />
           <Link to="/cart" className="ml-4">🛒MY CART</Link>
         </div>
       </div>
--- frontend/src/utils/getProductsByNameOrBrand.js
+++ frontend/src/utils/getProductsByNameOrBrand.js
@@ -2,58 +2,20 @@ import axios from "axios";
 
 export const getProductsByNameOrBrand = async ({
   search,
-  setLoading,
-  setError,
   navigate,
   setProducts,
 }) => {
   try {
-    if (search.length > 3) {
-      setLoading(true);
-      setError(null);
+    if (search !== "" || search.length > 3) {
       const response = await axios.get(
         `http://localhost:5000/products?brand_or_name=${search}`
       );
       navigate("/shop");
       setTimeout(() => {
         setProducts(response.data);
-        setLoading(false);
       }, 1000);
     }
   } catch (error) {
-    setError("Error searching for products:", error);
-    setLoading(false);
-  }
-};
-
-export const getProductsByNameOrBrandOnchange = async ({
-  search,
-  setLoading,
-  setError,
-  navigate,
-  setProducts,
-  searchResults,
-}) => {
-  try {
-    setLoading(true);
-    setError(null);
-    console.log(search);
-    if (search !== "") {
-      const response = await axios.get(
-        `http://localhost:5000/products?brand_or_name=${search}`
-      );
-      navigate("/shop");
-      setTimeout(() => {
-        setProducts(response.data);
-        setLoading(false);
-      }, 1000);
-    } else {
-      setProducts([]);
-      console.log(searchResults);
-      setLoading(false);
-    }
-  } catch (error) {
-    setError("Error searching for products:", error);
-    setLoading(false);
+    console.log("Error searching for products:", error);
   }
 };
