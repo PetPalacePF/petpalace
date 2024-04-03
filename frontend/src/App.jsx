1bd2d94926b97d66835ea1fa16c5699aece89f4b
nicolaspratg <nicoprat@hotmail.com>
2024-04-03 17:48:33 -0300

    agregue sort by price al shop y movi los sort

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,73 +15,97 @@ import Header from "./components/Header/Header";
 import AdminPanel from "./Views/Admin/AdminPanel";
 
 // * Utils
-import getCategories from './utils/getCategories.js'
+import getCategories from "./utils/getCategories.js";
 import { Shop } from "./Views/Shop.jsx";
 
 function App() {
-
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
-  const [filterCategories, setFilterCategories] = useState([])
-  const [filterPrice, setFilterPrice] = useState([])
+  const [filterCategories, setFilterCategories] = useState([]);
+  const [filterPrice, setFilterPrice] = useState([]);
   const [sortRating, setSortRating] = useState("");
+  const [sortPrice, setSortPrice] = useState("");
   // const [error, setError] = useState(null);
   // const [loading, setLoading] = useState(false);
   const [allCategories, setAllCategories] = useState({
     allIds: [],
     byId: {},
     loading: false,
-    error: ''
-  })
+    error: "",
+  });
 
   useEffect(() => {
-
     const get = async () => {
-      setAllCategories({ ...allCategories, loading: true })
-      const data = await getCategories()
-      setAllCategories({ ...allCategories, loading: false })
+      setAllCategories({ ...allCategories, loading: true });
+      const data = await getCategories();
+      setAllCategories({ ...allCategories, loading: false });
 
-      if (data.message) return setAllCategories({ ...allCategories, error: data.message })
+      if (data.message)
+        return setAllCategories({ ...allCategories, error: data.message });
 
-      const allIds = data.map(category => category.id)
+      const allIds = data.map((category) => category.id);
       const byId = data.reduce((acc, category) => {
-        acc[category.id] = category
-        return acc
-      }, {})
+        acc[category.id] = category;
+        return acc;
+      }, {});
 
       setAllCategories({
         allIds,
         byId,
         loading: false,
-        error: ''
-      })
-    }
+        error: "",
+      });
+    };
 
-    get()
-
-  }, [])
+    get();
+  }, []);
 
   return (
     <div className="">
       <Routes>
-        <Route path='/*' element={<>
-          <Header allCategories={allCategories} setProducts={setProducts} search={search} setSearch={setSearch} />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/detail/:id" element={<Detail />}></Route>
-            <Route path="/shop" element={<Shop
-              allCategories={allCategories}
-              search={search} setSearch={setSearch}
-              products={products} setProducts={setProducts}
-              filterCategories={filterCategories} setFilterCategories={setFilterCategories}
-              filterPrice={filterPrice} setFilterPrice={setFilterPrice}
-              sortRating={sortRating} setSortRating={setSortRating}
-            />}></Route>
-          </Routes>
-        </>} />
+        <Route
+          path="/*"
+          element={
+            <>
+              <Header
+                allCategories={allCategories}
+                setProducts={setProducts}
+                search={search}
+                setSearch={setSearch}
+              />
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/detail/:id" element={<Detail />}></Route>
+                <Route
+                  path="/shop"
+                  element={
+                    <Shop
+                      allCategories={allCategories}
+                      search={search}
+                      setSearch={setSearch}
+                      products={products}
+                      setProducts={setProducts}
+                      filterCategories={filterCategories}
+                      setFilterCategories={setFilterCategories}
+                      filterPrice={filterPrice}
+                      setFilterPrice={setFilterPrice}
+                      sortRating={sortRating}
+                      setSortRating={setSortRating}
+                      sortPrice={sortPrice}
+                      setSortPrice={setSortPrice}
+                    />
+                  }
+                ></Route>
+              </Routes>
+            </>
+          }
+        />
 
-        <Route path='/admin/*' element={<AdminPanel allCategories={allCategories} />} />
+        <Route
+          path="/admin/*"
+          element={<AdminPanel allCategories={allCategories} />}
+        />
       </Routes>
     </div>
   );
