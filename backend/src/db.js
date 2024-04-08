3447c2e34a9d2766989946fd295bff019160959e
Fernando <fer.cantora90@gmail.com>
2024-04-08 16:20:03 -0300

    commit before pull

--- backend/src/db.js
+++ backend/src/db.js
@@ -11,15 +11,16 @@ const Order_ProductModel = require("./models/Order_Product");
 
 //? CONNECTION
 const dataBase = new Sequelize(
-  DB_DEPLOY,
+  // DB_DEPLOY,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${HOST}:${PORT}/${DB_NAME}`,
   {
     logging: false,
     native: false,
-    dialectOptions:{
-      ssl:{
-        require: true,
-      }
-    }
+    // dialectOptions:{
+    //   ssl:{
+    //     require: true,
+    //   }
+    // }
   }
 );
 
