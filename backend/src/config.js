2b6ed890d2ab103747031b565f9f7e6d7b859901
Facundo Besio <facubesio.08@gmail.com>
2024-04-11 12:26:19 -0300

    Update config back

--- backend/src/config.js
+++ backend/src/config.js
@@ -10,7 +10,7 @@ const DB_DEPLOY =
   process.env.DB_DEPLOY ||
   `postgres://${DB_USER}:${DB_PASSWORD}@${HOST}:${PORT}/${DB_NAME}`;
 
-//const URL= "https://petpalace-lilac.vercel.app"; //? ESTA LA URL QUE HAY QUE PONER EN LAS VARIABLES DE ENTONORNO DEL DEPLOY BACK, COMO FRONTEND_URL.
+
 
 module.exports = {
   FRONTEND_URL,
@@ -19,6 +19,5 @@ module.exports = {
   HOST,
   PORT,
   DB_NAME,
-  DB_DEPLOY,
-  URL
+  DB_DEPLOY
 };
