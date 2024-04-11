d76e55a9c803141a3c8d39697e22b2d768966ba7
adrielmachado <adrielmachadodev@gmail.com>
2024-04-11 16:17:46 -0300

    delete carts listo

--- frontend/src/hooks/orders/useGetOrdersData.jsx
+++ frontend/src/hooks/orders/useGetOrdersData.jsx
@@ -5,13 +5,17 @@ import axios from "../../config/axios";
 const useGetOrdersData = () => {
   const [ordersData, setOrdersData] = useState({ orders: [] });
 
-  // useEffect(() => {
-  //   axios
-  //     .get("/orders")
-  //     .then((res) => res.data)
-  //     .then((data) => setOrdersData(data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  const userId = JSON.parse(window.localStorage.getItem("userData"))
+
+  useEffect(() => {
+    axios
+      .get(`/orders?filterUsers=${userId.id}`)
+      .then((res) => res.data)
+      .then((data) => {
+        setOrdersData(data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return {
     ordersData,
