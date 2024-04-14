009917fe0bd4e74a90e35d698379c6d0a01d567e
JoseMartinez0 <joseignacio2534@gmail.com>
2024-04-14 14:33:30 -0300

    cancelUrl

--- backend/src/routes/routerControllers/PaymentSession/postPaymentSession.js
+++ backend/src/routes/routerControllers/PaymentSession/postPaymentSession.js
@@ -1,18 +1,10 @@
 const createPaymentSession = require("../../../controllers/PaymentSession/createPaymentSession")
 
 const postPaymentSession = async(req,res) =>{
-<<<<<<< HEAD
-  const {products, origin} = req.body;
-  
-
-    try {
-        const session = await createPaymentSession(products,origin);
-=======
   const {products, origin} = req.body
 
     try {
         const session = await createPaymentSession(products, origin);
->>>>>>> 47963d2b11c9ec7966c14b73f2a425bd681ab965
         res.status(200).json({ sessionId: session.id });
       } catch (error) {
         console.error('Error al crear la sesión de pago:', error);
