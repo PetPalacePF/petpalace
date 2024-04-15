ab83a69ad8f79ce6cbebe5fa52aeb0692e401e2e
CastroNicolas <maximonicolascastro@gmail.com>
2024-04-15 14:45:27 +0200

    Change Components Footers

--- frontend/src/components/Footer/FooterNav.jsx
+++ frontend/src/components/Footer/FooterNav.jsx
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom"
 
 export const FooterNav = () => {
   return (
-         <div className="flex flex-col items-start mb-4">
-                    <Link to='/about' className="text-white mb-2"> ABOUT US </Link>
-                    <Link to='/shop' className="text-white mb-2"> SHOP </Link>
-                    <Link to='/dogs' className="text-white mb-2"> DOGS </Link>
-                    <Link to='/cats' className="text-white mb-2"> CATS </Link>
-                </div>
+    <div className="flex flex-col items-start mb-4">
+      <Link to='/about' className="text-white mb-2"> ABOUT US </Link>
+      <Link to='/contact' className="text-white mb-2"> CONTACT US </Link>
+      <Link to='/shop' className="text-white mb-2"> SHOP </Link>
+    </div>
   )
 }
