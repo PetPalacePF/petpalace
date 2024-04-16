3828b222ae9c088dcd531bc3183fed2873ee6173
CastroNicolas <maximonicolascastro@gmail.com>
2024-04-16 02:59:53 +0200

    My Ordes Complete

--- frontend/src/utils/getPurchases.js
+++ frontend/src/utils/getPurchases.js
@@ -6,6 +6,7 @@ const getPurchasefilterUsers = async ({ userId }, setPurchases) => {
       if (userId) {
         const response = await axios.get(`/purchases?filterUsers=${userId}`);
         setPurchases(response.data.purchases);
+        // setPurchases(response.data.purchases[0].Orders[0].products);
       }
     } catch (error) {
       console.error("Error fetching purchases:", error);
