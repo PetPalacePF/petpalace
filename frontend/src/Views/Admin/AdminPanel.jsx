8bdc42ae7cf9d31971bfe413438f86e81adc04ca
CastroNicolas <maximonicolascastro@gmail.com>
2024-04-17 21:51:20 +0200

    Cant set the purchases

--- frontend/src/Views/Admin/AdminPanel.jsx
+++ frontend/src/Views/Admin/AdminPanel.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Routes, Route } from "react-router-dom"
-import { Link } from "react-router-dom"
 
 // * Routes
-import Dashboard from './Dashboard.jsx'
 import Categories from './Categories.jsx'
 import Products from './Products.jsx'
 import Orders from './Orders.jsx'
@@ -20,12 +18,11 @@ const AdminPanel = ({ allCategories, setAllCategories }) => {
       <AdminNavbar />
       <div className="w-full ml-[200px] p-6">
         <Routes>
-          <Route path='/' element={<Dashboard />} />
           <Route path='/categories' element={<Categories allCategories={allCategories} setAllCategories={setAllCategories} />} />
           <Route path='/products' element={<Products />} />
           <Route path='/products/new' element={<ProductForm />} />
           <Route path='/Purchase' element={<Orders />} />
-          <Route path='/users' element={<Users/>} />
+          <Route path='/users' element={<Users />} />
         </Routes>
       </div>
     </div>
