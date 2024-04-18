8a98710651a1231c1419527e0c1658c910ba6e9b
nicolaspratg <nicoprat@hotmail.com>
2024-04-18 01:40:32 -0300

    epigraph responsive

--- frontend/src/Views/Epigraph.jsx
+++ frontend/src/Views/Epigraph.jsx
@@ -1,19 +1,27 @@
 export const Epigraph = () => {
   return (
-    <div className="bg-violetahome p-3 mb-10 flex justify-center">
-      <div className="flex flex-wrap justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-24 max-w-[1400px] gap-[140px]">
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
-          Free shipping
-        </p>
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
-          Easy payment
-        </p>
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
-          Quick return
-        </p>
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
-          Quality assurance
-        </p>
+    <div className="bg-violetahome p-3 mb-10 flex flex-wrap justify-center">
+      <div className="flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-24 max-w-[1400px] gap-4 sm:gap-8 md:gap-12 lg:gap-20">
+        <div className="flex flex-col items-center mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
+          <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white">
+            Free shipping
+          </p>
+        </div>
+        <div className="flex flex-col items-center mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
+          <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white">
+            Easy payment
+          </p>
+        </div>
+        <div className="flex flex-col items-center mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
+          <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white">
+            Quick return
+          </p>
+        </div>
+        <div className="flex flex-col items-center mb-2 sm:mb-0 flex-grow sm:flex-shrink-0 sm:flex-basis">
+          <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl font-bold text-white">
+            Quality assurance
+          </p>
+        </div>
       </div>
     </div>
   );
